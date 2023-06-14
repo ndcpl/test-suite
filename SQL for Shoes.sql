7840a83eedf96c984cbf6af37eeb6f889a57b11a
JiangHuPZ <93489186+JiangHuPZ@users.noreply.github.com>
2023-06-13 21:14:50 -0400

    sql file

---
+++ SQL for Shoes.sql
@@ -0,0 +1,19 @@
+create database `shoesWebsite`;
+use `shoesWebsite`;
+create table `memberData` (
+  `memberID` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(20) DEFAULT NULL,
+  `memberEmail` varchar(50) NOT NULL,
+  `memberFName` varchar(20) NOT NULL,
+  `memberLName` varchar(20) NOT NULL,
+  `memberAddress` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`memberID`)
+);
+Create table `customerService`(
+`inquiryID` int NOT NULL auto_increment,
+`INQFName` varchar(20) NOT NULL,
+`INQLName` varchar(20) NOT NULL,
+`INQEmail` varchar(50) NOT NULL,
+`inquiry` varchar(500) NOT NULL,
+PRIMARY KEY (`inquiryID`)
+);
